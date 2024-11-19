d77131db646f21ce65edfa240965a531989c8c8f
JEunseo <jjeseo1010@gmail.com>
2024-11-19 09:56:11 +0900

    [level 0] Title: qr code, Time: 0.14 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -3,10 +3,8 @@ import java.util.*;
 class Solution {
     public String solution(int q, int r, String code) {
         StringBuilder answer = new StringBuilder();
-        for(int i = 0; i < code.length(); i++){
-            if(i % q == r){
+        for(int i = r; i < code.length(); i += q){
                 answer.append(code.charAt(i));
-            }
         }
         return answer.toString();
     }
