9d288ab71dbd6694821e4b7b913fc64608557fc1
JEunseo <jjeseo1010@gmail.com>
2024-11-19 09:43:17 +0900

    [level 0] Title: 세로 읽기, Time: 0.17 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -2,17 +2,10 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string, int m, int c) {
-        List<String> answer = new ArrayList<>();
-        for(int i = 0; i < my_string.length(); i += m){
-            String sub = my_string.substring(i, i+m);
-            answer.add(sub);
+        StringBuilder answer = new StringBuilder();
+        for(int i = c-1; i < my_string.length(); i += m){
+            answer.append(my_string.charAt(i));
         }
-        
-        StringBuilder result = new StringBuilder();
-        for(String str : answer){
-            result.append(str.substring(c-1, c));
-        }
-        
-        return result.toString();
+        return answer.toString();
     }
 }
