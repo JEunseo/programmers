3a64e10be684ae6575584b312a0874cd87eb96bd
JEunseo <jjeseo1010@gmail.com>
2024-11-26 20:57:07 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        for(int num : numbers){
+            answer += num; 
+            if(answer > n){
+                return answer;
+            }
+        }
+        return answer;
+    }
+}
