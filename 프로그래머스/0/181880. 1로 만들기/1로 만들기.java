9defbe72b233ca7922914af52bbeb030b5daed6c
JEunseo <jjeseo1010@gmail.com>
2024-11-28 13:21:49 +0900

    [level 0] Title: 1로 만들기, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int num = 0;
+        
+        for(int i = 0; i < num_list.length; i++){
+            while(num_list[i] != 1){
+                num_list[i] = (num_list[i] % 2 == 0) ? num_list[i] / 2 : (num_list[i] -1) / 2;
+                num++;
+        }
+      }
+        return num;
+    }
+}
