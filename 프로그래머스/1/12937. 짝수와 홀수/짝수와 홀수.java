ce9bf5c3abc30b3d88f818821bcdb0e2acbb0756
JEunseo <jjeseo1010@gmail.com>
2024-12-01 11:18:52 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(int num) {
-        String answer = "";
-        return answer = num % 2 == 0 ? "Even" : "Odd";
+        return num % 2 == 0 ? "Even" : "Odd";
     }
 }
