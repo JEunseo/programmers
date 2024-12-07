cacbe82d85e2e45ae9a62a2b3090b53ca511e279
JEunseo <jjeseo1010@gmail.com>
2024-12-07 09:49:59 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int total = 45;
+        int sum = 0;
+        
+        for(int num : numbers){
+            sum += num;
+        }
+        
+        return total-sum;
+    }
+}
