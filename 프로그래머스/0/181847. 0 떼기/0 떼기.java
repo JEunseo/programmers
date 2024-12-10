c32c0a989a87ef2df33e16f543f6b65d481cf0eb
JEunseo <jjeseo1010@gmail.com>
2024-12-10 17:12:19 +0900

    [level 0] Title: 0 떼기, Time: 0.07 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String n_str) {
+        int idx = Integer.parseInt(n_str);
+        String answer = Integer.toString(idx);
+        return answer;
+    }
+}
