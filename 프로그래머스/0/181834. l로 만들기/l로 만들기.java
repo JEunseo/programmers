65d8e94f272f62eaefb9fd20cf2ccdf1d3e9804b
JEunseo <jjeseo1010@gmail.com>
2024-12-12 09:53:45 +0900

    [level 0] Title: l로 만들기, Time: 9.99 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String myString) {
+        StringBuilder answer = new StringBuilder();
+ 
+        for(char ch : myString.toCharArray()){
+            if(ch < 'l'){
+                answer.append("l");
+            } else {
+                answer.append(ch);
+            }
+        }
+        return answer.toString();
+    }
+}
