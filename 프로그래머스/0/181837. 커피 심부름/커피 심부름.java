d78f9e81a8d7c01f299257d085af7a4efbf15773
JEunseo <jjeseo1010@gmail.com>
2024-12-12 09:15:25 +0900

    [level 0] Title: 커피 심부름, Time: 1.33 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -2,12 +2,8 @@ class Solution {
     public int solution(String[] order) {
         int answer = 0;
         for(String menu : order){
-            if(menu.contains("cafelatte")){
-                answer += 5000;
-            } else {
-                answer += 4500;
-            }
-        }
+         answer += menu.contains("cafelatte") ? 5000 : 4500;
+            } 
         return answer;
+        }
     }
-}
